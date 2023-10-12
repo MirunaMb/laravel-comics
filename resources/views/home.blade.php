6bd5a27b47e93acf90bc48ce102664d39c1293ce
MirunaMb <mirunamarcela98@gmail.com>
2023-10-12 23:57:25 +0200

    messaggio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('page-title', 'Homepage')
+
+@section('main content')
+    <section>
+        <h1>Homepage</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, tempora?</p>
+    </section>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/comics', function () {
+    $comics=config('db.comics');
+    return view('comics',compact('comics'));
+})->name('comics');
+
